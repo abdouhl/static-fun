923aff3b4522e6cd869d5e57829ab3c206792487
abderrahmane <73205653+abdouhl@users.noreply.github.com>
2024-04-27 04:01:15 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,7 @@
 import faunadb from 'faunadb';
 
 export const client = new faunadb.Client({
-  secret: process.env.FAUNADB_STATIC_FUN_KEY
+  secret: process.env.FAUNADB_STATIC_FUN_KEY,
+  domain: 'db.fauna.com',
+  scheme: 'https',
 });
